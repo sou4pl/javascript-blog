cf29c6c80298c3f91256b362bca72dd844fc2438
Michał Słota <michal.k.slota@gmail.com>
2022-08-30 19:56:39 +0200

    add removing active class from articles

--- js/script.js
+++ js/script.js
@@ -6,10 +6,21 @@ function titleClickHandler(event){
 
   // remove class 'acitve' from all article links
 
+const activeLinks = document.querySelectorAll('.titles a.active')
+
+for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+}
+
   // add class 'active to the cliked link
 
-  // remowe class 'active from all articles
+  // remove class 'active from all articles
+  
+const activeArticles = document.querySelectorAll('.post.active')
 
+for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+}
   // get 'href' attribute form the clicked link
 
   // find the correct article using the selector (value of 'href' attribute)
